43889bce913c4d91c83bad0e1f8690455daec962
Eugen <Briam@mail.ru>
2017-04-24 14:02:27 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,6 +6,9 @@ public class Main
 	{
 		MyArray ar1 = new MyArray();
 		System.out.println(ar1.aIsSubsetB());
+		System.out.println(ar1.sum());
+		System.out.println(ar1.intersection());
 		System.out.println(ar1.diff());
+		System.out.println(ar1.simmetricDiff());
 	}
 }
