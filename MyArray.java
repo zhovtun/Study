2a03bc357bc13390c8ef1408d293e16f6362c626
Eugen <Briam@mail.ru>
2017-04-25 17:22:12 +0800

    Update MyArray.java

--- MyArray.java
+++ MyArray.java
@@ -1,205 +1,225 @@
+package ru.time2store.dmath;
+
 import java.util.Arrays;
 import java.lang.String;
 
 public class MyArray
 {
-	private String[] arr1; 
-	private String[] arr2;
-	private String[] arr3;
-	 
-	 MyArray (String subsetA, String subsetB) {
-		 arr1 = clean(subsetA);
-		 arr2 = clean(subsetB);
-		 arr3 = new String[arr1.length+arr2.length];
-	 } 
-	 
-	 public String[] clean (String str) {
-		 int i =0, j=0;
-		 String a = "a";
-		 String[] ch = str.split("");
-		 String[] array = new String[ch.length];
-		 
-		 while (i < ch.length) {
-			 if (a.compareTo(ch[i])>-26 && a.compareTo(ch[i])<1) {
-				 array[j] = ch[i];
-				 j++;
-				 i++;
-			 }
-			 else{i++;}
-		 }
-		 return array;
-	}
-	 
-	
-	 public String printArray (String[] arr) {
-		 int i=0;
-		 String result = "{ ";
-		 while (i < arr.length && arr[i] != null) {
-			 result += arr[i] + " ";
-			 i++;
-		 }
-		 result += "}";
-		 return result;
-	 }
-	 
-	 
-	 public void reset(String[] arr) {
-		 int i = 0;
-		 while(i<arr.length) {
-			 arr[i] = null;
-			 i++;
-		 }
-	 }
-	 
-	 
-	 public String aIsSubsetB () {  //а является подмножеством в
-	     int i=0, j=0;
-		 String result;
-		 
-		 while (i!=arr1.length && j!= arr2.length) {
-			 if (arr1[i] == arr2[j]) {
-				 i++;
-				 j++;
-			 }
-			 else {
-				 j++;
-			 }
-		 }
-		 if (i == arr1.length) {
-			 return result = "Множество А является подмножеством В";
-		 }
-		 else {
-			 return result = "Множество А не является подмножеством В";
-		 }
-     }
-	 
-	 
-	public String sum () {  //объединение множеств
-	    int i=0, j=0, k=0;
-		String result = " ";
-		reset(arr3);
-		
-		
-		if (aIsSubsetB() == "Множество А является подмножеством В") {
-			result = "Результат объединения - множество С = " + printArray(arr2);
-			}
-		else {
-			while (i!=arr1.length && j!= arr2.length && arr1[i] != null && arr2[j] != null) {
-				if (arr1[i].compareTo(arr2[j]) == 0) {
-				    arr3[k] = arr1[i];
-				    i++; 
-				    j++;
-				    k++;
-				}
-				else if (arr1[i].compareTo(arr2[j]) > 0) {
-					arr3[k] = arr2[j];
-					j++;
-					k++;
-				}
-				else {
-					arr3[k] = arr1[i];
-					i++;
-					k++;
-				}
-			}
-			while (i<arr1.length) {
-				arr3[k] = arr1[i];
-				k++;
-				i++;
-			}
-			result = "Результат объединения - множество С = " + printArray(arr3);
-		}
-		
-		return result;
-	}
-	
-	
-	public String intersection () {  //умножение (пересечение) множеств
-		int i=0, j=0, k=0;
-		String result = " ";
-		reset(arr3);
-		
-	    while (i!=arr1.length && j!= arr2.length && arr1[i] != null && arr2[j] != null) {
-			if (arr1[i].compareTo(arr2[j]) == 0) {
-				arr3[k] = arr1[i];
-				i++; 
-				j++;
-				k++;
-			}
-			else if (arr1[i].compareTo(arr2[j]) > 0) {
-				j++;
-			}
-			else {
-				i++;
-			}
-		}
-	    if (k == 0) {result = "Множества не пересакаются";}
-		else {result = "Результат пересечения - множество С = " + printArray(arr3);}	
-		return result;
-	}
-	
-	
-	public String diff () {  //разность множеств
-		int i=0, j=0, k=0;
-		String result = " ";
-		reset(arr3);
+    private String[] arr1;
+    private String[] arr2;
+    private String[] arr3;
 
-	    while (i!=arr1.length && j!= arr2.length && arr1[i] != null && arr2[j] != null) {
-			if (arr1[i].compareTo(arr2[j]) == 0) {
-				i++; 
-				j++;
-			}
-			else if (arr1[i].compareTo(arr2[j]) > 0) {
-				
-				j++;
-			}
-			else {
-				arr3[k] = arr1[i];
-				i++;
-				k++;
-			}
-		}
-		while (i<arr1.length) {
-			arr3[k] = arr1[i];
-			k++;
-			i++;
-		}
-		if (k == 0) {result = "Множества не вычитаются";}
-		else {result = "Результат разности - множество С = " + printArray(arr3);}	
-		return result;
-	}
-	
-	
-	public String simmetricDiff () {  //симметрическая разность
-		int i=0, j=0, k=0;
-		String result = " ";
-		reset(arr3);
+    MyArray () {
+        arr1 = new String[30];
+        arr2 = new String[30];
+        arr3 = new String[arr1.length+arr2.length];
+    }
 
-	    while (i<arr1.length && j< arr2.length && arr1[i] != null && arr2[j] != null) {
-			if (arr1[i].compareTo(arr2[j]) == 0) {
-				i++; 
-				j++;
-			}
-			else if (arr1[i].compareTo(arr2[j]) > 0) {
-				arr3[k] = arr2[j];
-				k++;
-				j++;
-			}
-			else {
-				arr3[k] = arr1[i];
-				i++;
-				k++;
-			}
-		}
-		while (i<arr1.length) {
-			arr3[k] = arr1[i];
-			k++;
-			i++;
-		}
-		if (k == 0) {result = "Множества не вычитаются";}
-		else {result = "Результат симметрической разности - множество С = " + printArray(arr3);}	
-		return result;
-	}
-	
-	
+
+    public void setArray (String str1, String str2) {
+        arr1 = clean (str1);
+        arr2 = clean (str2);
+        arr3 = new String[arr1.length + arr2.length];
+    }
+
+    public String[] clean (String str) {
+        int i =0, j=0, k=0;
+        String a = "a";
+        String[] ch = str.split("");
+        String[] array = new String[ch.length];
+
+        while (i < ch.length) {
+            if (a.compareTo(ch[i])>-26 && a.compareTo(ch[i])<1) {
+                array[j] = ch[i];
+                j++;
+                i++;
+            }
+            else{i++;}
+        }
+
+        while (array[k] != null) {k++;}
+        return array;
+    }
+
+    private void sort (String[] array) {
+        int i = 0, j = 0;
+        String temp;
+        while (array[i] != null) {
+            
+        }
+
+    }
+
+
+    public String printArray (String[] arr) {
+        int i=0;
+        String result = "{ ";
+        while (i < arr.length && arr[i] != null) {
+            result += arr[i] + " ";
+            i++;
+        }
+        result += "}";
+        return result;
+    }
+
+
+    public void reset(String[] arr) {
+        int i = 0;
+        while(i<arr.length) {
+            arr[i] = null;
+            i++;
+        }
+    }
+
+
+    public String aIsSubsetB () {  //а является подмножеством в
+        int i=0, j=0;
+        String result;
+
+        while (i!=arr1.length && j!= arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+        if (i == arr1.length) {
+            return result = "Множество А является подмножеством В";
+        }
+        else {
+            return result = "Множество А не является подмножеством В";
+        }
+    }
+
+
+    public String sum () {  //объединение множеств
+        int i=0, j=0, k=0;
+        String result = " ";
+        reset(arr3);
+
+
+        if (aIsSubsetB() == "Множество А является подмножеством В") {
+            result = "Результат объединения - множество С = " + printArray(arr2);
+        }
+        else {
+            while (i!=arr1.length && j!= arr2.length && arr1[i] != null && arr2[j] != null) {
+                if (arr1[i].compareTo(arr2[j]) == 0) {
+                    arr3[k] = arr1[i];
+                    i++;
+                    j++;
+                    k++;
+                }
+                else if (arr1[i].compareTo(arr2[j]) > 0) {
+                    arr3[k] = arr2[j];
+                    j++;
+                    k++;
+                }
+                else {
+                    arr3[k] = arr1[i];
+                    i++;
+                    k++;
+                }
+            }
+            while (i<arr1.length) {
+                arr3[k] = arr1[i];
+                k++;
+                i++;
+            }
+            result = "Результат объединения - множество С = " + printArray(arr3);
+        }
+
+        return result;
+    }
+
+
+    public String intersection () {  //умножение (пересечение) множеств
+        int i=0, j=0, k=0;
+        String result = " ";
+        reset(arr3);
+
+        while (i!=arr1.length && j!= arr2.length && arr1[i] != null && arr2[j] != null) {
+            if (arr1[i].compareTo(arr2[j]) == 0) {
+                arr3[k] = arr1[i];
+                i++;
+                j++;
+                k++;
+            }
+            else if (arr1[i].compareTo(arr2[j]) > 0) {
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        if (k == 0) {result = "Множества не пересакаются";}
+        else {result = "Результат пересечения - множество С = " + printArray(arr3);}
+        return result;
+    }
+
+
+    public String diff () {  //разность множеств
+        int i=0, j=0, k=0;
+        String result = " ";
+        reset(arr3);
+
+        while (i!=arr1.length && j!= arr2.length && arr1[i] != null && arr2[j] != null) {
+            if (arr1[i].compareTo(arr2[j]) == 0) {
+                i++;
+                j++;
+            }
+            else if (arr1[i].compareTo(arr2[j]) > 0) {
+
+                j++;
+            }
+            else {
+                arr3[k] = arr1[i];
+                i++;
+                k++;
+            }
+        }
+        while (i<arr1.length) {
+            arr3[k] = arr1[i];
+            k++;
+            i++;
+        }
+        if (k == 0) {result = "Множества не вычитаются";}
+        else {result = "Результат разности - множество С = " + printArray(arr3);}
+        return result;
+    }
+
+
+    public String simmetricDiff () {  //симметрическая разность
+        int i=0, j=0, k=0;
+        String result = " ";
+        reset(arr3);
+
+        while (i<arr1.length && j< arr2.length && arr1[i] != null && arr2[j] != null) {
+            if (arr1[i].compareTo(arr2[j]) == 0) {
+                i++;
+                j++;
+            }
+            else if (arr1[i].compareTo(arr2[j]) > 0) {
+                arr3[k] = arr2[j];
+                k++;
+                j++;
+            }
+            else {
+                arr3[k] = arr1[i];
+                i++;
+                k++;
+            }
+        }
+        while (i<arr1.length) {
+            arr3[k] = arr1[i];
+            k++;
+            i++;
+        }
+        if (k == 0) {result = "Множества не вычитаются";}
+        else {result = "Результат симметрической разности - множество С = " + printArray(arr3);}
+        return result;
+    }
+
+
 }
